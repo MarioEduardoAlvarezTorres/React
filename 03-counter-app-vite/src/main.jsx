6c2b3ee7aeb139ee0492702332291f78c13ebfb4
MarioEduardo <eduardoat44@gmail.com>
2022-09-27 23:36:35 -0500

    Creacion de Primeros componentes

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import { FirstApp } from './FirstApp';
+import { App } from './HelloWorldApp'
 
-function App(){
-    //document.createElement.....
-    return <h1>Hola Mundo!!!</h1>;
-}
-
+FirstApp
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App/>
+        <FirstApp/>
     </React.StrictMode>
 );
