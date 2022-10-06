01792bfefcf23a9577c6989b1d9f6ea6d6ef146a
MarioEduardo <eduardoat44@gmail.com>
2022-10-06 13:58:31 -0500

    Uso de primer hook

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) =>{
-    const handleAdd = (event) =>{
-        console.log(event)
+    
+    const [ countert,setCounter ] = useState(value);
+
+    const handleAdd = () =>{
+        setCounter(countert+1);
     }
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { countert } </h2>
             <button onClick={ handleAdd }>
-                1+1
+                +1
             </button>
         </>
     );
