05811c9381c1626d641a3b239c7c63d9a1086da7
MarioEduardo <eduardoat44@gmail.com>
2022-10-17 09:48:39 -0500

    Probar FirstApp

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 export const FirstApp = ({title,subTitle}) =>{
     return(
         <>
-            <h1> { title } </h1>
+            <h1>{ title }</h1>
             <p> { subTitle } </p>
         </>
     );     
--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -2,6 +2,14 @@ import {render} from '@testing-library/react';
 import { FirstApp } from '../src/FirstApp';
 describe('Pruebas en </FirstApp>', () => {
     test('debe de hacer match con el snapshot', () => {
-        render(<FirstApp title='hola' subTitle='hola'/>)
+        const {container} = render(<FirstApp title='hola' subTitle='hola'/>)
+        expect(container).toMatchSnapshot();
+    });
+
+    test('debe de mostrar el titulo en un h1',()=>{
+        const title = 'hola';
+        const {container,getByText} = render(<FirstApp title={title} subTitle='hola'/>)
+        const h1= container.querySelector('h1');
+        expect(h1.innerHTML).toBe(title)
     });
 });
