19c8482ba30c6df03940d59eba9b9dccaefab086
MarioEduardo <eduardoat44@gmail.com>
2022-10-17 11:27:51 -0500

    Creando una lista de categorias

--- 04-git-expert-app/src/GifExpertApp.jsx
+++ 04-git-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { useState } from "react";
 
 export const GifExpertApp = () =>{
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+    console.log(categories)
     return(
         <>
+            {/*titulo*/}
             <h1>GifExpertApp</h1>
+            {/*Input*/}
+            {/*Listado de Gif*/}
+            <ol>
+                {categories.map(category =>{
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
+                {/*Gif Item*/}
         </>
     )
 }
