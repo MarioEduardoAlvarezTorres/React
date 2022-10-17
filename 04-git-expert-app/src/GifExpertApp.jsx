8698a5d9f9e57955bf880e916010eb11ddab55b8
MarioEduardo <eduardoat44@gmail.com>
2022-10-17 11:33:40 -0500

    Agregar una ueva categoria

--- 04-git-expert-app/src/GifExpertApp.jsx
+++ 04-git-expert-app/src/GifExpertApp.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 export const GifExpertApp = () =>{
     const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
-    console.log(categories)
+    const onAddCategory = () =>{
+        setCategories([...categories,'Valorant'])
+        //setCategories(cat => [...cat,'Valorant'])
+    }
     return(
         <>
             {/*titulo*/}
             <h1>GifExpertApp</h1>
             {/*Input*/}
             {/*Listado de Gif*/}
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map(category =>{
                     return <li key={category}>{category}</li>
