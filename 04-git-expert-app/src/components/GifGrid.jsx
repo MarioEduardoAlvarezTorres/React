4d3df863cf67ec7c86d531a7cdd66cc0808b5eca
MarioEduardo <eduardoat44@gmail.com>
2022-10-19 14:04:20 -0500

    Uso de useEffect

--- 04-git-expert-app/src/components/GifGrid.jsx
+++ 04-git-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({category}) => {
-    getGifs(category);
+    const [counter, setCounter] = useState(10)
+    useEffect(() => {
+      getGifs(category);
+    }, [])
+    
+   
     return (
     <>
         <h3>{category}</h3>
+        <h5>{counter}</h5>
         <p>Hola Mundo</p>
+        <button onClick={()=>setCounter(counter + 1)}>+1</button>
     </>
   )
 }
